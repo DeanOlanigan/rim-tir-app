8569aed1d49bcca277b795b048ebfc141df5422d
Сергей Савватеев <huawei.honor228873@gmail.com>
2024-12-11 18:25:03 +0700

    Login page on ChakraUi & gradient port

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,18 +1,22 @@
 import { SHA1 } from "crypto-js";
 import PropTypes from "prop-types";
-import { Button, Input, Stack, Container } from "@chakra-ui/react";
+import { Button, Input, Stack, Box, AbsoluteCenter, Alert, Card } from "@chakra-ui/react";
 import { Field } from "../../components/ui/field";
 import { PasswordInput } from "../../components/ui/password-input";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import Gradient from "./Gradient";
+import "./LoginPage.css";
 
 function LoginForm({ onLogin }) {
+    const [sharedError, setSharedError] = useState(null);
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm();
 
-    const onSubmit = handleSubmit(async (data) => {
+    const onSubmit = async (data) => {
         let hashPassword = SHA1(data.password).toString();
         try {
             const response = await fetch("/api/v1/login", {
@@ -46,45 +50,65 @@ function LoginForm({ onLogin }) {
 
                 onLogin();
             } else {
-                const result = await response.json();
-                alert(result.message);
+                const errorData = await response.json();
+                setSharedError(errorData.message);
             }
 
         } catch (error) {
-            console.error(error);
+            setSharedError(error.message);
         }
-    });
+    };
 
     return (
-        <Container>
-            <form onSubmit={onSubmit}>
-                <Stack>
-                    <Field
-                        label="Username"
-                        invalid={!!errors.username}
-                        errorText={errors.username?.message}
-                    >
-                        <Input
-                            {...register("username", {
-                                required: "Username is required",
-                            })}
-                        />
-                    </Field>
-                    <Field
-                        label="Password"
-                        invalid={!!errors.password}
-                        errorText={errors.password?.message}
-                    >
-                        <PasswordInput
-                            {...register("password", {
-                                required: "Password is required",
-                            })}
-                        />
-                    </Field>
-                    <Button type="submit">Login</Button>
-                </Stack>
-            </form>
-        </Container>
+        <Box position={"relative"} h={"100vh"}>
+            <Gradient />
+            <AbsoluteCenter axis={"both"}>
+                <Card.Root w={"sm"} bg="bg/50" border={"none"}>
+                    <Card.Body>
+                        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                            <Stack>
+                                <Field
+                                    label="Логин"
+                                    required
+                                    invalid={!!errors.username}
+                                    errorText={errors.username?.message}
+                                >
+                                    <Input
+                                        autoComplete="off"
+                                        {...register("username", {
+                                            required: "Вы должны ввести логин",
+                                        })}
+                                    />
+                                </Field>
+                                <Field
+                                    label="Пароль"
+                                    required
+                                    invalid={!!errors.password}
+                                    errorText={errors.password?.message}
+                                >
+                                    <PasswordInput
+                                        {...register("password", {
+                                            required: "Вы должны ввести пароль",
+                                        })}
+                                    />
+                                </Field>
+                                {sharedError && (
+                        
+                                    <Box>
+                                        <Alert.Root status={"error"}>
+                                            <Alert.Indicator />
+                                            <Alert.Title>{sharedError}</Alert.Title>
+                                        </Alert.Root>
+                                    </Box>
+                        
+                                )}
+                                <Button size={"xs"} type="submit">Войти</Button>
+                            </Stack>
+                        </form>
+                    </Card.Body>
+                </Card.Root>
+            </AbsoluteCenter>
+        </Box>
     );
 }
 LoginForm.propTypes = {
