8c0f882359092d7d873a0b5762f1d2484c795b9d
Сергей Савватеев <huawei.honor228873@gmail.com>
2024-12-14 17:02:55 +0700

    toaster & session notify

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,110 @@ import { BrowserRouter as Router} from "react-router-dom";
 import Header from "./components/Header/Header";
 import AppRoutes from "./routes/AppRoutes";
 import WebSocketProvider from "./providers/WebSocketProvider";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import LoginForm from "./pages/LoginPage/LoginPage";
+import { Toaster, toaster } from "./components/ui/toaster";
+import {
+    DialogBody,
+    DialogCloseTrigger,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogRoot,
+    DialogTitle,
+    DialogActionTrigger
+} from "./components/ui/dialog";
+import { Button } from "@chakra-ui/react";
+
+function Dialog(props) {
+    return (
+        <DialogRoot role="alertdialog" defaultOpen>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Session expired soon</DialogTitle>
+                </DialogHeader>
+                <DialogBody>
+                    {props.children}
+                </DialogBody>
+                <DialogFooter>
+                    <DialogActionTrigger asChild>
+                        <Button variant='outline'>Cancel</Button>
+                    </DialogActionTrigger>
+                </DialogFooter>
+                <DialogCloseTrigger/>
+            </DialogContent>
+        </DialogRoot>
+    );
+}
 
 function App() {
     const [isAuth, setIsAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const toastId = useRef();
+    const [toasterReady, setToasterReady] = useState(false);
 
     useEffect(() => {
+        const toasterTimeout = setTimeout(() => setToasterReady(true), 5000);
+
         const checkSession = () => {
             const currentTime = Math.floor(Date.now() / 1000);
             const sessionExpirationTime = localStorage.getItem("session_expiration_time");
-            let timeLeft = sessionExpirationTime - currentTime;
-            console.log(`checkSession: ${timeLeft}`);
             if (sessionExpirationTime && currentTime < parseInt(sessionExpirationTime, 10)) {
+                const remainingTime = sessionExpirationTime - currentTime;
+                console.log("Remaining time:", remainingTime);
                 setIsAuth(true);
+
+                if (toasterReady && remainingTime < 2680) {
+                    console.log(toastId.current);
+                    if (!toastId.current) {
+                        toastId.current = toaster.create({
+                            title: "Session expired soon",
+                            description: `Your session will expire in ${remainingTime} seconds.`,
+                            type: "warning",
+                            duration: 60000,
+                            action: {
+                                label: "Extend session",
+                                onClick: () => {
+                                    localStorage.setItem("session_expiration_time", parseInt(sessionExpirationTime, 10) + 1440);
+                                    toaster.dismiss(toastId.current);
+                                    toastId.current = "";
+                                }
+                            }
+                        });
+                    } else {
+                        toaster.update(toastId.current, {
+                            description: `Your session will expire in ${remainingTime} seconds.`,
+                        });
+                    }
+                }
+
             } else {
                 setIsAuth(false);
                 localStorage.removeItem("session_expiration_time");
                 localStorage.removeItem("csrf");
+                
+                if (toastId.current) {
+                    toaster.dismiss(toastId.current);
+                    toastId.current = "";
+                }
             }
         };
         checkSession();
-        const interval = setInterval(checkSession, 30000);
-        return () => clearInterval(interval);
-    }, []);
+        setLoading(false);
+        const interval = setInterval(checkSession, 1000);
+        return () => {
+            clearTimeout(toasterTimeout); 
+            clearInterval(interval);
+        };
+    }, [toasterReady]);
 
     const handleLogin = () => {
         setIsAuth(true);
     };
+
+    if (loading) {
+        return <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "black" }}>Loading...</div>;
+    }
 
     if (!isAuth) {
         return <LoginForm onLogin={handleLogin}/>;
@@ -40,6 +116,7 @@ function App() {
         <WebSocketProvider>
             <Router>
                 <div style={{display: "flex", flexDirection: "column", height: "100vh"}}>
+                    <Toaster />
                     <Header />
                     <main style={{ height: "100%", paddingTop: "1rem" }}>
                         <AppRoutes />
