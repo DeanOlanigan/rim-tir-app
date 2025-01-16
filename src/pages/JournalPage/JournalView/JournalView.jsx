5db3122ab410ca105b0cb4142aea588790406133
DeanOLanigan <145742378+DeanOlanigan@users.noreply.github.com>
2025-01-16 16:45:39 +0700

    journal virt table

--- src/pages/JournalPage/JournalView/JournalView.jsx
+++ src/pages/JournalPage/JournalView/JournalView.jsx
@@ -1,4 +1,4 @@
-import { Card, Table, Flex, IconButton, Link, Box, CheckboxGroup } from "@chakra-ui/react";
+import { Card, Flex, IconButton, Link, Box, CheckboxGroup } from "@chakra-ui/react";
 import { Checkbox } from "../../../components/ui/checkbox";
 import {
     MenuContent,
@@ -8,17 +8,47 @@ import {
 import { LuPause, LuPlay, LuDownload } from "react-icons/lu";
 import { useJournalContext } from "../../../providers/JournalProvider/JournalContext";
 import { tableColumns } from "../JournalFilter/filterOptions";
+import { memo, useMemo, useRef } from "react";
+import {
+    AutoSizer,
+    Table,
+    Column,
+    CellMeasurer,
+    CellMeasurerCache
+} from "react-virtualized";
+import "react-virtualized/styles.css";
+
+const columnsConfig = [
+    { dataKey: "date", label: "Дата" },
+    { dataKey: "type", label: "Тип" },
+    { dataKey: "var", label: "Переменная" },
+    { dataKey: "desc", label: "Описание" },
+    { dataKey: "val", label: "Значение" },
+    { dataKey: "group", label: "Группа" },
+];
 
 function JournalView() {
     const { isPaused, journalHeaders, journalRows, setIsPaused, setHeaders } = useJournalContext();
     console.log("Render JournalView");
     const tableHeadersMap = {
-        date: "Дата и время",
-        type: "Тип",
-        var: "Переменная",
-        desc: "Описание",
-        val: "Значение",
-        group: "Группа"
+        date: { label: "Дата и время", minWidth: "100px", maxWidth: "120px" },
+        type: { label: "Тип", minWidth: "145px", maxWidth: "100px" },
+        var: { label:"Переменная", minWidth: "100px", maxWidth: "100px" },
+        desc: { label: "Описание", minWidth: "100px", maxWidth: "100px" },
+        val: { label: "Значение", minWidth: "100px", maxWidth: "100px" },
+        group: { label: "Группа", minWidth: "100px", maxWidth: "100px" }
+    };
+    
+    const rowData = {
+        journalHeaders,
+        journalRows,
+        tableHeadersMap
+    };
+
+    const RowRenderer = ({ index, style }) => {
+        return (
+            <JournalRow index={index} style={style} data={rowData}/>
+        );
     };
 
     return (
@@ -68,42 +98,188 @@ function JournalView() {
                 </Flex>
             </Card.Header>
             <Card.Body h={"100%"} overflow={"auto"} pt={"0"} mt={"1rem"}>
-                
-                <Table.Root size={"sm"} interactive stickyHeader>
-                    <Table.Header>
-                        <Table.Row justifyContent={"space-between"}>
+                <Box
+                    w={"100%"}
+                    h={"100%"}
+                >
+                    <DynamicTable rows={journalRows} columns={columnsConfig}/>
+                </Box>
+                {/* <Box 
+                    h={"100%"}
+                    w={"100%"}
+                    display={"table"}
+                    tableLayout={"fixed"}
+                >
+                    <Box display={"table-header-group"}>
+                        <Box display={"table-row"}>
                             {
                                 journalHeaders.map((item) => {
                                     return (
-                                        <Table.ColumnHeader textAlign={"center"} key={item}>
-                                            {tableHeadersMap[item]}
-                                        </Table.ColumnHeader>
+                                        <Box
+                                            key={item}
+                                            display={"table-cell"}
+                                            textAlign={"start"}
+                                            fontWeight={"medium"}
+                                            py={"3"}
+                                            minW={tableHeadersMap[item].minWidth}
+                                            maxW={tableHeadersMap[item].maxWidth}
+                                        >
+                                            {tableHeadersMap[item].label}
+                                        </Box>
                                     );
                                 })
                             }
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {
-                            journalRows.map((item, index) => {
-                                return (
-                                    <Table.Row key={index} background={item.mark ? "green.200" : ""}>
-                                        {
-                                            journalHeaders.map((column)=> {
-                                                return (
-                                                    <Table.Cell textAlign={"center"} key={column}>{item[column]}</Table.Cell>
-                                                );
-                                            })
-                                        }
-                                    </Table.Row>
-                                );
-                            })
-                        }
-                    </Table.Body>
-                </Table.Root>
-
+                        </Box>
+                    </Box>
+                    <Box display={"table-row-group"}>
+                        <AutoSizer>
+                            {({ width, height }) => (
+                                <List
+                                    height={height}
+                                    itemCount={journalRows.length}
+                                    itemSize={60}
+                                    width={width}
+                                >
+                                    {RowRenderer}
+                                </List>
+                            )}
+                        </AutoSizer>
+                    </Box>
+                </Box> */}
+                {
+                    /* journalRows.map((item, index) => {
+                        return (
+                            <Table.Row as={"div"} key={index} background={item.mark ? "green.200" : ""}>
+                                {
+                                    journalHeaders.map((column)=> {
+                                        return (
+                                            <Table.Cell as={"div"} textAlign={"center"} key={column}>{item[column]}</Table.Cell>
+                                        );
+                                    })
+                                }
+                            </Table.Row>
+                        );
+                    }) */
+                }
             </Card.Body>
         </Card.Root>
+    );
+}
+
+const JournalRow = memo(function JournalRow({index, style, data}) {
+    const item = data.journalRows[index];
+    if (!item) return null;
+
+    return (
+        
+        <Flex
+            style={style}
+            display={"table-row"}
+            background={item.mark ? "green.200" : ""}
+        >
+            {
+                data.journalHeaders.map((column) => (
+                    <Box
+                        key={column}
+                        textAlign={"start"}
+                        display={"table-cell"}
+                        borderBottom={"1px solid black"}
+                        px={"1"}
+                        py={"1"}
+                        minW={data.tableHeadersMap[column].minWidth}
+                        maxW={data.tableHeadersMap[column].maxWidth}
+                    >
+                        {item[column]}
+                    </Box>
+                ))
+            }
+        </Flex>
+        
+    );
+});
+
+function DynamicTable({ columns = columnsConfig, rows }) {
+    const cache = useMemo(() => {
+        return new CellMeasurerCache({
+            fixedHeight: true,
+            minWidth: 100, 
+        });
+    }, []);
+
+    const tableRef = useRef(null);
+
+    const cellRenderer = ({
+        dataKey,
+        columnIndex,
+        rowIndex,
+        key,
+        parent,
+        style 
+    }) => {
+
+        const rowData = rows[rowIndex];
+        const cellValue = rowData[dataKey];
+
+        return (
+            <CellMeasurer
+                key={key}
+                cache={cache}
+                columnIndex={columnIndex}
+                rowIndex={rowIndex}
+                parent={parent}
+            >
+                {({measure}) => (
+                    <div
+                        style={{
+                            ...style,
+                            whiteSpace: "nowrap", // чтобы строка не переносилась
+                            overflow: "visible",  // или hidden, если хотите обрезать
+                            textOverflow: "ellipsis"
+                        }}
+                        onLoad={measure} // если вдруг есть картинки, measure нужно дергать по факту загрузки
+                    >
+                        {cellValue}
+                    </div>
+                )}
+            </CellMeasurer>
+        );
+    };
+
+    const rowGetter = ({index}) => rows[index];
+
+    return (
+        <div>
+            <AutoSizer>
+                {({height, width}) => (
+                    <Table
+                        ref={tableRef}
+                        width={width}
+                        height={500}
+                        headerHeight={30}
+                        rowHeight={cache.rowHeight}
+                        rowCount={rows.length}
+                        rowGetter={rowGetter}
+                        deferredMeasurementCache={cache}
+                    >
+                        {columns.map((col,columnIndex) => (
+                            <Column
+                                key={col.dataKey}
+                                dataKey={col.dataKey}
+                                label={col.label}
+                                width={100}
+                                flexGrow={1}
+                                cellRenderer={(props) => 
+                                    cellRenderer({
+                                        ...props,
+                                        columnIndex
+                                    })
+                                }
+                            />
+                        ))}
+                    </Table>
+                )}
+            </AutoSizer>
+        </div>
     );
 }
 
