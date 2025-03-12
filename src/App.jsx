300774489a7c9c53ff4431f8f0a996eb8e858ba9
Сергей Савватеев <huawei.honor228873@gmail.com>
2025-03-12 19:03:28 +0700

    removed unnecessary dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Provider } from "./components/ui/provider";
 import AppRoutes from "./routes/AppRoutes";
 import "./App.css";
-import "prismjs/themes/prism-tomorrow.css";
 
 function App() {
     return (
