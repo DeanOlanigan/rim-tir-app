9d5a9902f61a23819d12f8c006b892bbf397d49b
DeanOLanigan <145742378+DeanOlanigan@users.noreply.github.com>
2025-03-27 15:20:37 +0700

    baseinput selectinput update

--- src/pages/ConfigurationPage/InputComponents/SelectInput.jsx
+++ src/pages/ConfigurationPage/InputComponents/SelectInput.jsx
@@ -8,19 +8,21 @@ import {
     SelectItem,
     SelectLabel,
 } from "../../../components/ui/select";
+import { Select } from "@chakra-ui/react";
 import { PARAM_DEFINITIONS } from "../../../config/paramDefinitions";
 
 export const SelectInput = memo(function SelectInput(props) {
     //console.log("Render SelectInput");
-    const { targetKey, id, value, showLabel = false } = props;
+    const { targetKey, id, value, showLabel = false, ...rest } = props;
     const setSettings = useVariablesStore((state) => state.setSettings);
     const label = PARAM_DEFINITIONS[targetKey].label;
     const collection = PARAM_DEFINITIONS[targetKey].options;
 
     return (
-        <SelectRoot
+        <Select.Root
             maxW={"250px"}
             size={"xs"}
+            {...rest}
             collection={collection}
             value={[value]}
             onValueChange={(details) => {
@@ -28,20 +30,30 @@ export const SelectInput = memo(function SelectInput(props) {
                     [targetKey]: details.value[0],
                 });
             }}
+            positioning={{ sameWidth: true, placement: "bottom" }}
         >
-            {showLabel && <SelectLabel>{label}</SelectLabel>}
-            <SelectTrigger>
-                <SelectValueText
-                    placeholder={`Выберите ${label.toLowerCase()}`}
-                />
-            </SelectTrigger>
-            <SelectContent>
-                {collection?.items.map((row) => (
-                    <SelectItem item={row} key={row.value}>
-                        {row.label}
-                    </SelectItem>
-                ))}
-            </SelectContent>
-        </SelectRoot>
+            {showLabel && <Select.Label>{label}</Select.Label>}
+            <Select.HiddenSelect />
+            <Select.Control>
+                <Select.Trigger>
+                    <Select.ValueText
+                        placeholder={`Выберите ${label.toLowerCase()}`}
+                    />
+                </Select.Trigger>
+                <Select.IndicatorGroup>
+                    <Select.Indicator />
+                </Select.IndicatorGroup>
+            </Select.Control>
+            <Select.Positioner>
+                <Select.Content>
+                    {collection?.items.map((row) => (
+                        <Select.Item item={row} key={row.value}>
+                            {row.label}
+                            <Select.ItemIndicator />
+                        </Select.Item>
+                    ))}
+                </Select.Content>
+            </Select.Positioner>
+        </Select.Root>
     );
 });
