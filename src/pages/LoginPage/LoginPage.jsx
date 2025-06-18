bfba31f54876871e71794140353747e0cdde79b3
DeanOLanigan <145742378+DeanOlanigan@users.noreply.github.com>
2025-06-18 09:46:58 +0700

    login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,22 +3,65 @@ import {
     Input,
     Stack,
     Box,
-    AbsoluteCenter,
     Alert,
-    Card,
+    Grid,
+    Flex,
+    VStack,
+    Heading,
+    Text,
+    Fieldset,
+    Field,
 } from "@chakra-ui/react";
 import { Button } from "@/components/ui/button";
-import { Field } from "@/components/ui/field";
 import { PasswordInput } from "@/components/ui/password-input";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import Gradient from "@/components/GradientBackground/GradientBackground";
 import { LuLogIn } from "react-icons/lu";
 import { Navigate } from "react-router-dom";
 import { useAuthContext } from "@/providers/AuthProvider/AuthContext";
 import Lightning from "@/components/Lightning/Lightning";
 
 function LoginForm() {
+    const { isAuthenticated } = useAuthContext();
+
+    console.log("Render LoginForm; isAuthenticated:", isAuthenticated);
+
+    if (isAuthenticated) {
+        return <Navigate to="/configuration" replace />;
+    }
+
+    return (
+        <Grid minH={"100svh"} templateColumns={{ base: "1fr", lg: "1fr 1fr" }}>
+            {/* <Gradient /> */}
+            <Box display={{ base: "none", lg: "block" }} position={"relative"}>
+                <Lightning
+                    hue={220}
+                    xOffset={0}
+                    speed={0.6}
+                    intensity={0.4}
+                    size={1}
+                />
+            </Box>
+            <Flex
+                direction={"column"}
+                gap={"4"}
+                p={{ base: "6", md: "12" }}
+                bg={"bg.muted"}
+            >
+                <Flex flex={"1"} align={"center"} justify={"center"}>
+                    <Box w={"100%"} maxW={"xs"}>
+                        <LoginCard />
+                    </Box>
+                </Flex>
+            </Flex>
+        </Grid>
+    );
+}
+
+export default LoginForm;
+
+const LoginCard = () => {
+    const { login } = useAuthContext();
     const [loading, setLoading] = useState(false);
     const [sharedMessage, setSharedMessage] = useState({
         type: null,
@@ -29,10 +72,6 @@ function LoginForm() {
         handleSubmit,
         formState: { errors },
     } = useForm();
-    const { login, isAuthenticated } = useAuthContext();
-
-    console.log("Render LoginForm; isAuthenticated:", isAuthenticated);
-
     const onSubmit = async (data) => {
         setLoading(true);
         let hashPassword = SHA1(data.password).toString();
@@ -76,101 +115,66 @@ function LoginForm() {
             setSharedMessage({ type: "error", message: error.message });
         }
     };
-
-    if (isAuthenticated) {
-        return <Navigate to="/configuration" replace />;
-    }
-
     return (
-        <Box position={"relative"} h={"100vh"} w={"100vw"}>
-            {/* <Gradient /> */}
-
-            {/* <AbsoluteCenter> */}
-            <Box
-                position={"absolute"}
-                top={"50%"}
-                right={"25%"}
-                zIndex={1}
-                display={"flex"}
-                alignItems={"center"}
-                justifyContent={"center"}
-            >
-                <Card.Root
-                    w={"sm"}
-                    bg="bg/50"
-                    border={"none"}
-                    position={"absolute"}
-                    zIndex={1}
-                >
-                    <Card.Body>
-                        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-                            <Stack>
-                                <Field
-                                    label="Логин"
-                                    required
-                                    invalid={!!errors.username}
-                                    errorText={errors.username?.message}
-                                >
-                                    <Input
-                                        autoComplete="off"
-                                        {...register("username", {
-                                            required: "Вы должны ввести логин",
-                                        })}
-                                    />
-                                </Field>
-                                <Field
-                                    label="Пароль"
-                                    required
-                                    invalid={!!errors.password}
-                                    errorText={errors.password?.message}
-                                >
-                                    <PasswordInput
-                                        {...register("password", {
-                                            required: "Вы должны ввести пароль",
-                                        })}
-                                    />
-                                </Field>
-                                <Box h={"50px"}>
-                                    {sharedMessage.type && (
-                                        <Alert.Root
-                                            status={sharedMessage.type}
-                                            data-state={"open"}
-                                            animationDuration={"slow"}
-                                            animationStyle={{
-                                                _open: "scale-fade-in",
-                                            }}
-                                        >
-                                            <Alert.Indicator />
-                                            <Alert.Title>
-                                                {sharedMessage.message}
-                                            </Alert.Title>
-                                        </Alert.Root>
-                                    )}
-                                </Box>
-                                <Button
-                                    loading={loading}
-                                    size={"xs"}
-                                    type="submit"
-                                >
-                                    <LuLogIn />
-                                    Войти
-                                </Button>
-                            </Stack>
-                        </form>
-                    </Card.Body>
-                </Card.Root>
-            </Box>
-
-            {/* </AbsoluteCenter> */}
-            <Lightning
-                hue={220}
-                xOffset={1.2}
-                speed={0.6}
-                intensity={0.4}
-                size={1}
-            />
-        </Box>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <Fieldset.Root size={"lg"}>
+                <VStack>
+                    <Fieldset.Legend>
+                        <Heading size={"3xl"}>Добро пожаловать!</Heading>
+                    </Fieldset.Legend>
+                    <Fieldset.HelperText fontSize={"md"} fontWeight={"medium"}>
+                        Введите логин и пароль
+                    </Fieldset.HelperText>
+                </VStack>
+                <Fieldset.Content>
+                    <Field.Root required invalid={!!errors.username}>
+                        <Field.Label>Логин</Field.Label>
+                        <Input
+                            autoComplete="off"
+                            {...register("username", {
+                                required: "Вы должны ввести логин",
+                            })}
+                        />
+                        {errors.username && (
+                            <Field.ErrorText>
+                                {errors.username.message}
+                            </Field.ErrorText>
+                        )}
+                    </Field.Root>
+                    <Field.Root required invalid={!!errors.password}>
+                        <Field.Label>Пароль</Field.Label>
+                        <PasswordInput
+                            {...register("password", {
+                                required: "Вы должны ввести пароль",
+                            })}
+                        />
+                        {errors.password && (
+                            <Field.ErrorText>
+                                {errors.password.message}
+                            </Field.ErrorText>
+                        )}
+                    </Field.Root>
+                </Fieldset.Content>
+                <Button loading={loading} size={"xs"} type="submit">
+                    <LuLogIn />
+                    Войти
+                </Button>
+                <Box h={"50px"}>
+                    {sharedMessage.type && (
+                        <Alert.Root
+                            status={sharedMessage.type}
+                            data-state={"open"}
+                            animationDuration={"slow"}
+                            animationStyle={{
+                                _open: "scale-fade-in",
+                            }}
+                        >
+                            <Alert.Indicator />
+                            <Alert.Title>{sharedMessage.message}</Alert.Title>
+                        </Alert.Root>
+                    )}
+                </Box>
+            </Fieldset.Root>
+        </form>
     );
-}
-
-export default LoginForm;
+};
