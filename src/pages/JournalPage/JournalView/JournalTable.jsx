d6576ba0f8bafceb927d15de9b6a848fe5b96437
Dean O'Lanigan <145742378+DeanOlanigan@users.noreply.github.com>
2025-10-09 14:10:13 +0700

    journal rework sample

--- src/pages/JournalPage/JournalView/JournalTable.jsx
+++ src/pages/JournalPage/JournalView/JournalTable.jsx
@@ -1,8 +1,10 @@
 import { Box, Text } from "@chakra-ui/react";
 import { useColorModeValue } from "@/components/ui/color-mode";
 import { AutoSizer, Table, Column } from "react-virtualized";
+import { useQuery } from "@tanstack/react-query";
+import { QK } from "@/api";
 
-function JournalTable({ scrollRef, columns, rows }) {
+export const JournalTable = ({ scrollRef, columns, rows }) => {
     const oddBackgroundColor = useColorModeValue("white", "#111111");
     const evenBackgroundColor = useColorModeValue("#e4e4e7", "#27272a");
 
@@ -113,5 +115,51 @@ function JournalTable({ scrollRef, columns, rows }) {
             </AutoSizer>
         </Box>
     );
-}
-export default JournalTable;
+};
+
+const useJournalHistory = () => {
+    const q = useQuery({
+        queryKey: QK.journal,
+        queryFn: async () => {
+            const out = [];
+            // eslint-disable-next-line
+            const count = Math.floor(Math.random() * 30);
+
+            for (let i = 0; i < count; i++) {
+                out.push({
+                    ts: Date.now(),
+                    // eslint-disable-next-line
+                    type: ["ts", "tu"][Math.floor(Math.random() * 2)],
+                    name: "test",
+                    // eslint-disable-next-line
+                    value: Math.floor(Math.random() * 100),
+                    group: ["noGroup", "danger", "warn", "state"][
+                        // eslint-disable-next-line
+                        Math.floor(Math.random() * 4)
+                    ],
+                });
+            }
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            return out;
+        },
+    });
+
+    return q;
+};
+
+export const TestTable = () => {
+    const { data, isLoading, isError, error } = useJournalHistory();
+
+    if (isLoading) return <Text>Loading...</Text>;
+    if (isError) return <Text>Error: {error.message}</Text>;
+
+    return (
+        <Box>
+            {data.map((item, index) => (
+                <Text
+                    key={index}
+                >{`${item.ts} | ${item.type} | ${item.group} | ${item.name} | ${item.value}`}</Text>
+            ))}
+        </Box>
+    );
+};
