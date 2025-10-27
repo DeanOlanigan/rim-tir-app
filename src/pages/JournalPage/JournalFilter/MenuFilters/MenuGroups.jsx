e55e7cf132de04395c970a23d1b6dd399a936bd2
wowobaldet <88originalll@gmail.com>
2025-10-27 20:00:38 +0700

    Added filtering variant with a menus for the Groups and Types

---
+++ src/pages/JournalPage/JournalFilter/MenuFilters/MenuGroups.jsx
@@ -0,0 +1,63 @@
+import { Box, Menu, Portal, useCheckboxGroup } from "@chakra-ui/react";
+import { flexRender } from "@tanstack/react-table";
+import { useGroupStore } from "../../JournalStores/GroupFilterStore";
+import { LuArrowBigDown } from "react-icons/lu";
+
+const groups = [
+    { label: "Без группы", value: "Без Группы" },
+    { label: "Аварийные", value: "Аварийная" },
+    { label: "Предупредительные", value: "Предупредительная" },
+    { label: "Оперативного состояния", value: "Состояние" },
+];
+
+export const MenuGroups = ({header}) => {
+    const {
+        selectedGroups,
+        setSelectedGroups
+    } = useGroupStore();
+    const group = useCheckboxGroup({ defaultValue: selectedGroups });
+    
+    const handleCheckboxChange = (value, checked) => {
+        const newColumns = checked 
+            ? [...selectedGroups, value]
+            : selectedGroups.filter(col => col !== value);
+    
+        setSelectedGroups(newColumns);
+    };
+
+    return (
+        <Menu.Root closeOnSelect={false}>
+            <Menu.Trigger asChild>
+                <Box cursor="pointer" display="inline-flex" alignItems="center">
+                    {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                    )}
+                    <LuArrowBigDown />
+                </Box>
+            </Menu.Trigger>
+            <Portal>
+                <Menu.Positioner>
+                    <Menu.Content>
+                        <Menu.ItemGroup>
+                            {groups.map(({ value, label }) => (
+                                <Menu.CheckboxItem
+                                    key={value}
+                                    value={value}
+                                    checked={group.isChecked(value)}
+                                    onCheckedChange={(checked) => {
+                                        handleCheckboxChange(value, checked);
+                                        group.toggleValue(value);
+                                    }}
+                                >
+                                    {label}
+                                    <Menu.ItemIndicator />
+                                </Menu.CheckboxItem>
+                            ))}
+                        </Menu.ItemGroup>
+                    </Menu.Content>
+                </Menu.Positioner>
+            </Portal>
+        </Menu.Root>
+    );
+};
