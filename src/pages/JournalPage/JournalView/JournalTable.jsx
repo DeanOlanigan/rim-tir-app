902ccf034141aa4cea966453096f1d7f81861b4f
wowobaldet <88originalll@gmail.com>
2025-10-30 18:39:28 +0700

    journal table hooks and deleted chakra table

--- src/pages/JournalPage/JournalView/JournalTable.jsx
+++ src/pages/JournalPage/JournalView/JournalTable.jsx
@@ -1,182 +1,195 @@
-import { Box, Text, Table, IconButton } from "@chakra-ui/react";
-import { useReactTable, getCoreRowModel, flexRender } from "@tanstack/react-table";
+import { Box, Text, IconButton } from "@chakra-ui/react";
 import { LuArrowDown } from "react-icons/lu";
-import { useColorModeValue } from "@/components/ui/color-mode";
-import { AutoSizer, Column } from "react-virtualized";
-import { useQuery } from "@tanstack/react-query";
-import { QK } from "@/api";
-import { useGraphStore } from "@/pages/GraphsPage/store/store";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { flexRender } from "@tanstack/react-table";
 import { useGroupStore } from "../JournalStores/GroupFilterStore";
 import { useMessageFilterStore } from "../JournalStores/MessageFilterStore";
 import { useColumnsStore } from "../JournalStores/ColumnsStore";
-import { useEffect, useMemo, useRef } from "react";
-import { measureElement, useVirtualizer } from "@tanstack/react-virtual";
 import { useStickToBottom } from "use-stick-to-bottom";
+import { NoData } from "@/components/NoData";
+import { Loader } from "@/components/Loader";
+import { MenuGroups } from "../JournalFilter/MenuFilters/MenuGroups";
+import { MenuTypes } from "../JournalFilter/MenuFilters/MenuTypes";
+import { useJournalStream } from "../JournalStores/journal-stream-store";
+import { useJournalData } from "../hooks/useJournalData";
+import { useFilterData } from "../hooks/useFilterData";
+import { useFilterColumns } from "../hooks/useFilterColumns";
+import { useCreateTable } from "../hooks/useCreateTable";
 
 const tableColumns = [
-    { label: "Дата и время", value: "date" },
-    { label: "Тип", value: "type" },
-    { label: "Группа", value: "group" },
-    { label: "Переменная", value: "var" },
-    { label: "Значение", value: "val" },
-    { label: "Описание", value: "desc" },
+    { label: "Дата и время", value: "date", size: 200 },
+    { label: "Тип", value: "type", size: 80 },
+    { label: "Группа", value: "group", size: 120 },
+    { label: "Переменная", value: "var", size: 120 },
+    { label: "Значение", value: "val", size: 100 },
+    { label: "Описание", value: "desc", size: 200 },
 ];
 
-
-const useJournalHistory = () => {
-    const q = useQuery({
-        queryKey: ["journal"],
-        queryFn: async () => {
-            const out = [];
-            // eslint-disable-next-line
-            //const count = Math.floor(Math.random() * 30);
-            const count = 1000;
-
-            for (let i = 0; i < count; i++) {
-                out.push({
-                    date: new Date().toLocaleString(),
-                    // eslint-disable-next-line
-                    type: ["ts", "tu"][Math.floor(Math.random() * 2)],
-                    var: "test",
-                    // eslint-disable-next-line
-                    val: Math.floor(Math.random() * 100),
-                    group: ["noGroup", "danger", "warn", "state"][
-                        // eslint-disable-next-line
-                        Math.floor(Math.random() * 4)
-                    ],
-                    desc: "test Decrip"
-                });
-            }
-            await new Promise((resolve) => setTimeout(resolve, 1000))
-            console.log(out);
-            return out;
-        },
-    });
-
-    return q;
-};
-
-export const TestTables = () => {
-    const { data, isLoading, isError, error } = useJournalHistory();
+export const TestTable = () => {
+    const { isLoading, isError, error } = useJournalData();
     const selectedGroups = useGroupStore(state => state.selectedGroups);
-    const selectedMessages = useMessageFilterStore(state => state.selectedMessages) 
-    const tableColumnsZus = useColumnsStore(state => state.tableColumnsZus)
+    const selectedMessages = useMessageFilterStore(state => state.selectedMessages);
+    const tableColumnsZus = useColumnsStore(state => state.tableColumnsZus);
+    const {
+        live,
+    } = useJournalStream();
 
-    const FilterData = (data, selectedGroups, selectedMessages) => {
-        if (!data) return [];
-        const filteredDataFunc = data.filter((item) => {
-            if (!selectedGroups || !selectedMessages) return false;
-            return selectedGroups.includes(item.group) && selectedMessages.includes(item.type)
-        });
-        console.log(filteredDataFunc);
-        return filteredDataFunc;
-    };
-
-    const FilterColumns = (tableColumns, tableColumnsZus) => {
-        const filteredColumnsFunc = tableColumns.filter((colon) => {
-            if (!tableColumnsZus) return false;
-            return tableColumnsZus.includes(colon.value); 
-        });
-        return filteredColumnsFunc;
-    };
-
-    const filtredData = useMemo(() => FilterData(data, selectedGroups, selectedMessages), [data, selectedGroups, selectedMessages])
-
-    const filtreColon = useMemo(() => FilterColumns(tableColumns, tableColumnsZus), [tableColumnsZus])
-
-    const sticky = useStickToBottom();
-
-    const columns = useMemo(() => 
-        filtreColon.map(column => ({
-            accessorKey: column.value,
-            header: column.label,
-            size: column.size,
-            cell: ({ getValue }) => (
-                <Text fontSize="sm">{getValue()}</Text>
-            ),
-        })),
-    [filtreColon]
-    );
-
-    const table = useReactTable({
-        data: filtredData,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
+    const sticky = useStickToBottom({
+        initial: "instant",
+        resize: "instant"
     });
+
+    const filtredData = useFilterData(live, selectedGroups, selectedMessages);
+    const filtreColon = useFilterColumns(tableColumns, tableColumnsZus);
+
+    const table = useCreateTable(filtreColon, filtredData);
 
     const { rows } = table.getRowModel();
 
-    const tableVirualizer = useVirtualizer({
-        count: filtredData.length,
+    const virtualizer = useVirtualizer({
+        count: rows.length,
         getScrollElement: () => sticky.scrollRef.current,
         estimateSize: () => 34,
-        overscan: 20,
+        overscan: 5,
     });
 
-    const virtualRows = tableVirualizer.getVirtualItems();
+    const virtualRows = virtualizer.getVirtualItems();
 
-    if (isLoading) return <Text>Loading...</Text>;
+    if (isLoading) return (
+        <Loader text={"Загрузка данных"}/>
+    );
     if (isError) return <Text>Error: {error.message}</Text>;
 
-    return ( 
-        <Table.Root size="sm" stickyHeader ref={sticky.scrollRef} colorPalette={"current"}>
-            <thead style={{position: "sticky"}}>
-                {table.getHeaderGroups().map((headerGroup) => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map((header) => {
-                            return (
-                                <th
-                                    key={header.id}
-                                    colSpan={header.colSpan}
-                                    style={{ 
-                                        width: header.getSize(),
-                                        textAlign: "center",
-                                        padding: "8px",                       
-                                        fontWeight: "bold",
-                                        fontSize: "sm",
-                                        position: "sticky"
-                                    }}
-                                >
-                                    {header.isPlaceholder ? null : (
-                                        flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext(),
-                                        )
-                                    )}
-                                </th>
-                            );
-                        })}
-                    </tr>
-                ))}
-            </thead>
-            <tbody>
-                {virtualRows.map((virtualRow) => {
-                    const row = rows[virtualRow.index];
-                    return (
-                        <tr key={row.id}>
-                            {row.getVisibleCells().map((cell) => {
-                                return (
-                                    <td 
-                                        key={cell.id}
-                                        style={{
-                                            width: cell.column.getSize(),
+    return (
+        <Box
+            height="100%"
+            overflow={"hidden"}
+            borderRadius={"sm"}
+        >
+            {(filtredData.length === 0 || filtreColon.length === 0) && (
+                <NoData />
+            )}
+            <table style={{top: 0, left: 0,width: "100%" }}>
+                <thead>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id} >
+                            {headerGroup.headers.map((header) => {
+                                if (header.id === "group" || header.id === "type") return (
+                                    <Box as={"th"}
+                                        key={header.id}
+                                        colSpan={header.colSpan}
+                                        bg={"colorPalette.solid"}
+                                        style={{ 
+                                            width: header.getSize(),
                                             textAlign: "center",
-                                            padding: "8px",
-                                            borderBottom: "2px solid #f1f5f9",
+                                            padding: "8px",                       
+                                            fontWeight: "bold",
                                             fontSize: "sm",
-                                            fontWeight: "500",                                   
+                                            color: "white",
+                                        }}
+                                    >   
+                                        {header.id === "group"
+                                            ? <MenuGroups header={header}/>
+                                            : <MenuTypes header={header} />
+                                        }
+                                    </Box>
+                                );
+                                return (
+                                    <Box as={"th"}
+                                        key={header.id}
+                                        colSpan={header.colSpan}
+                                        bg={"colorPalette.solid"}
+                                        style={{ 
+                                            width: header.getSize(),
+                                            textAlign: "center",
+                                            padding: "8px",                       
+                                            fontWeight: "bold",
+                                            fontSize: "sm",
+                                            color: "white",
                                         }}
                                     >
-                                        {flexRender(
-                                            cell.column.columnDef.cell,
-                                            cell.getContext(),
+                                        {header.isPlaceholder ? null : (
+                                            <Box>
+                                                {flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext(),
+                                                )}
+                                            </Box>
                                         )}
-                                    </td>
+                                    </Box>
                                 );
                             })}
                         </tr>
-                    );})}
-            </tbody>
-        </Table.Root>
+                    ))}
+                </thead>
+            </table>
+            <Box ref={sticky.scrollRef} height="95%" overflow={"auto"}>
+                <Box 
+                    style={{ 
+                        height: `${virtualizer.getTotalSize()}px`, 
+                        position: "relative",
+                    }}
+                    ref={sticky.contentRef}
+                >
+                    {virtualRows.map((virtualRow) => {
+                        const row = rows[virtualRow.index];
+                        return (
+                            <Box
+                                key={row.id}
+                                style={{
+                                    position: "absolute",
+                                    top: 0,
+                                    left: 0,
+                                    height: `${virtualRow.size}px`,
+                                    transform: `translateY(${virtualRow.start}px)`,
+                                }}
+                            >
+                                <table style={{ width: "100%", tableLayout: "fixed" }}>
+                                    <tbody>
+                                        <tr>
+                                            {row.getVisibleCells().map((cell) => {
+                                                return (
+                                                    <td 
+                                                        key={cell.id}
+                                                        style={{
+                                                            width: cell.column.getSize(),
+                                                            textAlign: "center",
+                                                            padding: "5px",
+                                                            paddingLeft: "10px",
+                                                            borderBottom: "2px solid #f1f5f9",
+                                                            fontSize: "sm",
+                                                            fontWeight: "500",                                
+                                                        }}
+                                                    >
+                                                        {flexRender(
+                                                            cell.column.columnDef.cell,
+                                                            cell.getContext(),
+                                                        )}
+                                                    </td>
+                                                );
+                                            })}
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </Box>
+                        );
+                    })}
+                </Box>
+            </Box>
+            {!sticky.isAtBottom && (
+                <Box position="absolute" bottom="6" right="8" zIndex="10">
+                    <IconButton
+                        size="sm"
+                        onClick={() => {
+                            sticky.scrollToBottom("instant");
+                            virtualizer.measure();
+                        }}
+                        variant="solid"
+                    >
+                        <LuArrowDown />
+                    </IconButton>
+                </Box>
+            )}
+        </Box>
     );
 };
