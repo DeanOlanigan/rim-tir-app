217aabac93d5710e57474a543f822c9c8abeefb3
DeanOLanigan <145742378+DeanOlanigan@users.noreply.github.com>
2025-11-13 17:56:52 +0700

    more debug info

--- src/pages/HMIEditor/DebugInfo.jsx
+++ src/pages/HMIEditor/DebugInfo.jsx
@@ -15,14 +15,41 @@ export const DebugInfo = () => {
             bg={"bg"}
             w={"100%"}
             h={"100%"}
+            maxH={"250px"}
+            overflow={"auto"}
             borderRadius={"md"}
             shadow={"md"}
             p={2}
         >
             <Text fontWeight={"medium"}>DebugInfo</Text>
+
             <Text>Current action: {currentAction}</Text>
             <Text>Selected nodes: {selectedIds.length}</Text>
             <Text>Nodes: {Object.keys(nodes).length}</Text>
+            {selectedIds.length > 0 && (
+                <SelectedNodeInfo nodes={nodes} selectedIds={selectedIds} />
+            )}
+        </Box>
+    );
+};
+
+const SelectedNodeInfo = ({ nodes, selectedIds }) => {
+    function showParam(param) {
+        return selectedIds.length > 1
+            ? "Multiple"
+            : nodes[selectedIds[0]]?.[param];
+    }
+
+    return (
+        <Box>
+            <Text>Node: {showParam("type")}</Text>
+            <Text>Id: {showParam("id")}</Text>
+            <Text>X: {showParam("x")}</Text>
+            <Text>Y: {showParam("y")}</Text>
+            <Text>W: {showParam("width")}</Text>
+            <Text>H: {showParam("height")}</Text>
+            <Text>Rx: {showParam("radiusX")}</Text>
+            <Text>Ry: {showParam("radiusY")}</Text>
         </Box>
     );
 };
