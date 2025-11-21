40b9fb609a0ce5c001abb3532b41a20e2dfccfed
wowobaldet <88originalll@gmail.com>
2025-11-21 18:08:02 +0700

    Better file uploader

--- src/pages/SettingsPage/Settings/Updates.jsx
+++ src/pages/SettingsPage/Settings/Updates.jsx
@@ -1,12 +1,43 @@
 import {
+    Box,
+    Button,
     Card,
     FileUpload,
     Heading,
-    IconButton,
+    Icon,
     Text,
     Textarea,
+    useFileUploadContext,
 } from "@chakra-ui/react";
 import { LuUpload } from "react-icons/lu";
+
+const MAX_FILES = 1;
+
+const ConditionalDropzone = () => {
+    const fileUpload = useFileUploadContext();
+    const acceptedFiles = fileUpload.acceptedFiles;
+
+    if (acceptedFiles.length >= MAX_FILES) {
+        return null;
+    }
+
+    return (
+        <FileUpload.Dropzone>
+            <Icon size="md" color="fg.muted">
+                <LuUpload />
+            </Icon>
+            <FileUpload.DropzoneContent>
+                <Box>
+                    Перетащите файл обновления сюда или кликните по этой области
+                </Box>
+                <Box color="fg.muted">
+                    {MAX_FILES - acceptedFiles.length} файл для
+                    {MAX_FILES - acceptedFiles.length !== 1 ? "s" : ""} загрузки
+                </Box>
+            </FileUpload.DropzoneContent>
+        </FileUpload.Dropzone>
+    );
+};
 
 export const Updates = () => {
     return (
@@ -22,16 +53,23 @@ export const Updates = () => {
                     <Textarea
                         variant="subtle"
                         placeholder="Здесь будет виден процесс установки"
+                        fontWeight={"500"}
                     />
-                    <FileUpload.Root paddingTop="3">
+                    <FileUpload.Root
+                        paddingTop="3"
+                        w="100%"
+                        alignItems="stretch"
+                        maxFiles={MAX_FILES}
+                        accept={["exe"]}
+                    >
                         <FileUpload.HiddenInput />
-                        <FileUpload.Trigger asChild>
-                            <IconButton variant={"ghost"} size={"sm"}>
-                                <LuUpload /> Загрузите файл обновления
-                            </IconButton>
-                        </FileUpload.Trigger>
+                        <ConditionalDropzone />
+                        <FileUpload.List clearable />
                     </FileUpload.Root>
                 </Card.Body>
+                <Card.Footer>
+                    <Button>Установить</Button>
+                </Card.Footer>
             </Card.Root>
         </>
     );
