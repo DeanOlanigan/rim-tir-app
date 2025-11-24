8bb9e0fd80826c23682b06572b5530820ce4a855
wowobaldet <88originalll@gmail.com>
2025-11-24 18:28:25 +0700

    Beta roles for users

--- src/pages/SettingsPage/Settings/SettingsStore/user-edit-store.js
+++ src/pages/SettingsPage/Settings/SettingsStore/user-edit-store.js
@@ -1,8 +1,44 @@
 import { create } from "zustand";
 
+const Roles = [
+    {
+        label: "Админ",
+        value: "admin",
+        rights: [
+            { label: "Все права", value: "allrights" },
+        ],
+    },
+    {
+        label: "Водолаз",
+        value: "vodolaz",
+        rights: [
+            { label: "Доступ к журналированию", value: "journ-view"},
+            { label: "Доступ к логам", value: "log-view" },
+        ],
+    },
+    {
+        label: "Работяга",
+        value: "worker",
+        rights: [
+            { label: "Доступ к конфигурации", value: "conf-view" },
+            { label: "Редактирование данных в мониторинге", value: "monitor-edit" },
+            { label: "Доступ к графикам", value: "graph" },
+            { label: "Доступ к журналированию", value: "journ-view"},
+        ],
+    },
+    {
+        label: "В шоке",
+        value: "shoked",
+        rights: [
+            { label: "Доступ к конфигурации", value: "conf-view" },
+        ],
+    },
+];
 
 export const useEditStore = create(
     (set) => ({
+        roles: Roles,
+        createRole: (newRole) => set((state) => ({roles: {...state.roles, newRole}})),
         editedUsers: [{}],
         setEditedUsers: (newEditedUsers) => set(() => ({editedUsers: newEditedUsers}))
     }));
