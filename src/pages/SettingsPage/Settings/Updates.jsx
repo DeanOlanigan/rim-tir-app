29c14c4f9515b7c47e0fd5554a657b12032b368e
wowobaldet <88originalll@gmail.com>
2025-11-27 21:48:05 +0700

    test data for a downloading process, sending files

--- src/pages/SettingsPage/Settings/Updates.jsx
+++ src/pages/SettingsPage/Settings/Updates.jsx
@@ -1,3 +1,4 @@
+import { apiv2 } from "@/api/baseUrl";
 import {
     Box,
     Button,
@@ -9,9 +10,23 @@ import {
     Textarea,
     useFileUploadContext,
 } from "@chakra-ui/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import { LuUpload } from "react-icons/lu";
 
 const MAX_FILES = 1;
+
+const useUpdatesLogs = (enabled) => {
+    return useQuery({
+        queryKey: ["update"],
+        queryFn: async () => {
+            const res = await apiv2.get("/checkUpdate");
+            return res.data;
+        },
+        refetchInterval: 5000,
+        enabled,
+    });
+};
 
 const ConditionalDropzone = () => {
     const fileUpload = useFileUploadContext();
@@ -40,6 +55,32 @@ const ConditionalDropzone = () => {
 };
 
 export const Updates = () => {
+    const [downloaderFile, setDownloader] = useState();
+    const [isDown, setDown] = useState(false);
+    const [logs, setLogs] = useState([]);
+
+    const sendFileMutation = useMutation({
+        mutationKey: ["senderFile"],
+        mutationFn: async () => {
+            const formData = new FormData();
+            formData.append("downloader", downloaderFile[0]);
+            console.log(downloaderFile[0]);
+        },
+        onSuccess: () => {
+            setDown(!isDown);
+        },
+    });
+
+    const { data } = useUpdatesLogs(isDown);
+
+    useEffect(() => {
+        if (data?.message) {
+            setLogs((prev) => [...prev.concat(data.message)]);
+        }
+    }, [data]);
+
+    console.log(isDown);
+
     return (
         <>
             <Heading paddingBottom={"2"}>Обновления </Heading>
@@ -51,9 +92,12 @@ export const Updates = () => {
                 </Card.Header>
                 <Card.Body>
                     <Textarea
+                        value={logs.join("\n") || ""}
                         variant="subtle"
                         placeholder="Здесь будет виден процесс установки"
                         fontWeight={"500"}
+                        autoresize
+                        maxH="xl"
                     />
                     <FileUpload.Root
                         paddingTop="3"
@@ -61,7 +105,7 @@ export const Updates = () => {
                         alignItems="stretch"
                         maxFiles={MAX_FILES}
                         accept={[".ipk"]}
-                        onFileChange={(e) => console.log(e.acceptedFiles)}
+                        onFileChange={(e) => setDownloader(e.acceptedFiles)}
                     >
                         <FileUpload.HiddenInput />
                         <ConditionalDropzone />
@@ -69,7 +113,9 @@ export const Updates = () => {
                     </FileUpload.Root>
                 </Card.Body>
                 <Card.Footer>
-                    <Button>Установить</Button>
+                    <Button onClick={() => sendFileMutation.mutate()}>
+                        Установить
+                    </Button>
                 </Card.Footer>
             </Card.Root>
         </>
