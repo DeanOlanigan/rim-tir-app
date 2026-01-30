46f8e0588784b3abfcaec227256c099c3283aab5
DeanOLanigan <145742378+DeanOlanigan@users.noreply.github.com>
2026-01-30 17:50:16 +0700

    hide start coord marker in view only mode

--- src/pages/HMIEditor/canvas/HMICanvas.jsx
+++ src/pages/HMIEditor/canvas/HMICanvas.jsx
@@ -114,11 +114,13 @@ export const HMICanvas = ({
 };
 
 const StartCoords = () => {
+    const viewOnlyMode = useActionsStore((state) => state.viewOnlyMode);
     const showStartCoordMarker = useActionsStore(
         (state) => state.showStartCoordMarker,
     );
     return (
-        showStartCoordMarker && (
+        showStartCoordMarker &&
+        !viewOnlyMode && (
             <Star
                 x={0}
                 y={0}
