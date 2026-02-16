bf837652ff723bb5ed88eb045a47d7388beadb86
DeanOlanigan <145742378+DeanOlanigan@users.noreply.github.com>
2026-02-16 20:50:29 +0700

    remove redundant

--- src/pages/HMIEditor/store/commands/nodes/duplicateNodes.command.js
+++ src/pages/HMIEditor/store/commands/nodes/duplicateNodes.command.js
@@ -1,45 +1,6 @@
-import { buildIndexesFromNodes } from "../../utils/bindings";
 import { buildPayload } from "../../utils/clipboard";
-import { duplicateSubtrees } from "../../utils/nodes";
 import { buildInsertPatch } from "../clipboard/buildInsertPatch";
 import { runCommand } from "../runCommand";
-
-export const duplicateNodesCommandOLD = (api, ids) => {
-    runCommand(api, "cmd/nodes/duplicateNodes", (state) => {
-        const pageId = state.activePageId;
-        const page = state.pages[pageId];
-        if (!page) return null;
-
-        const { nodes, rootIds, selectedIds } = duplicateSubtrees(
-            state.nodes,
-            ids,
-        );
-
-        const updatedPage = {
-            ...page,
-            rootIds: [...page.rootIds, ...rootIds],
-        };
-
-        const { nodeIndex, varIndex } = buildIndexesFromNodes(nodes);
-
-        const patch = {
-            nodes,
-            pages: {
-                ...state.pages,
-                [pageId]: updatedPage,
-            },
-            varIndex,
-            nodeIndex,
-        };
-
-        return {
-            patch,
-            dirty: true,
-            selection: "set",
-            selectedIds,
-        };
-    });
-};
 
 export const duplicateNodesCommand = (api, ids, opts = {}) => {
     const gridSize = opts.gridSize ?? 1;
