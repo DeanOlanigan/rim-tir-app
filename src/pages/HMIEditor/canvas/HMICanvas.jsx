279ebad434e03d1ac03e2505439ab65a3fdbf16c
DeanOlanigan <145742378+DeanOlanigan@users.noreply.github.com>
2026-03-01 18:17:10 +0700

    move selection rects

--- src/pages/HMIEditor/canvas/HMICanvas.jsx
+++ src/pages/HMIEditor/canvas/HMICanvas.jsx
@@ -9,8 +9,8 @@ import { useToggleHitRegion } from "./hooks/useToggleHitRegion";
 import { useStageKeyboard } from "./hooks/useStageKeyboard";
 import { StartCoords } from "./StartCoords";
 import { Ruler } from "./Ruler";
-import { useEffectiveNode } from "../store/interactive-store";
-import { getNodeParentLocalAABB } from "../store/utils/geometry";
+import { SelectionRects } from "./SelectionRects";
+import { SelectionSize } from "./SelectionSize";
 
 export const HMICanvas = ({
     manager,
@@ -69,31 +69,9 @@ export const HMICanvas = ({
                     overviewRef={overviewLayerRef}
                 />
                 <SelectionRects />
+                <SelectionSize />
                 <Ruler />
             </Layer>
         </Stage>
-    );
-};
-
-const SelectionRects = () => {
-    const selectedIds = useNodeStore((state) => state.selectedIds);
-    if (selectedIds.length < 2) return null;
-    return selectedIds.map((id) => <SelectionRect key={id} id={id} />);
-};
-
-const SelectionRect = ({ id }) => {
-    const node = useEffectiveNode(id);
-    const aabb = getNodeParentLocalAABB(node);
-    return (
-        <Rect
-            x={aabb.x}
-            y={aabb.y}
-            width={aabb.width}
-            height={aabb.height}
-            stroke="rgb(0, 100, 255)"
-            strokeWidth={2}
-            strokeScaleEnabled={false}
-            listening={false}
-        />
     );
 };
