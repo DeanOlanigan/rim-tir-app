bd565ab4ab9e850e46901d277a1dd54c94134444
DeanOLanigan <145742378+DeanOlanigan@users.noreply.github.com>
2026-03-20 12:24:36 +0700

    persist sidebar

--- src/store/app-store.js
+++ src/store/app-store.js
@@ -1,12 +1,23 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useAppStore = create((set, get) => ({
-    fullScreenMode: false,
-    sideBarCollapsed: false,
-    toggleFullScreenMode: () => {
-        set({ fullScreenMode: !get().fullScreenMode });
-    },
-    toggleSideBarCollapsed: () => {
-        set({ sideBarCollapsed: !get().sideBarCollapsed });
-    },
-}));
+export const useAppStore = create(
+    persist(
+        (set, get) => ({
+            fullScreenMode: false,
+            sideBarCollapsed: false,
+            toggleFullScreenMode: () => {
+                set({ fullScreenMode: !get().fullScreenMode });
+            },
+            toggleSideBarCollapsed: () => {
+                set({ sideBarCollapsed: !get().sideBarCollapsed });
+            },
+        }),
+        {
+            name: "app-storage",
+            partialize: (state) => ({
+                sideBarCollapsed: state.sideBarCollapsed,
+            }),
+        },
+    ),
+);
